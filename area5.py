38864f9a4fc685d1df5fada5ebd74251085183ca
Chandanapriya01 <132696208+Chandanapriya01@users.noreply.github.com>
2023-05-18 19:47:12 +0530

    Add files via upload

---
+++ area5.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 17 21:46:18 2023
+
+@author: Chand
+"""
+rad = int(input("radius of the circle is: "))
+pi = 3.14
+area = int(pi)*rad*rad
+print("area of the circle is: ",area)
